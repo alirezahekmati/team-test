9c16cd26e54331f570cbd3fee48316d134a7cd91
Silvia <silvia.piovesan@gmail.com>
2022-04-02 15:23:26 +0200

    added Silvia

--- script.js
+++ script.js
@@ -3,41 +3,44 @@
                alireza section         
  **********************************************
  ************************************************ */
-let isAlirezaEmoji = false
+let isAlirezaEmoji = false;
 function alirezaEmojiClick(e) {
-    isAlirezaEmoji = !isAlirezaEmoji
-    isAlirezaEmoji ? e.target.textContent = "😳" : e.target.textContent = "😊"
+  isAlirezaEmoji = !isAlirezaEmoji;
+  isAlirezaEmoji ? (e.target.textContent = "😳") : (e.target.textContent = "😊");
 }
-document.getElementById("alireza-face").addEventListener("click", alirezaEmojiClick)
+document.getElementById("alireza-face").addEventListener("click", alirezaEmojiClick);
 
 /*******************************
  * Member Constructor
  *******************************/
 
- const memberData = {
-    sushant: { name: "Sushant", timeZone: "GMT + 5:30" },
+const memberData = {
+  sushant: { name: "Sushant", timeZone: "GMT + 5:30" },
+  silvia: { name: "Silvia", timeZone: "GMT + 1" },
+};
+
+function Member(memberDetails) {
+  Object.assign(this, memberDetails);
+
+  this.log = function () {
+    console.log(this.name);
   };
-  
-  function Member(memberDetails) {
-    Object.assign(this, memberDetails);
-  
-    this.log = function () {
-      console.log(this.name);
-    };
-  
-    this.addToPage = function () {
-      const p = document.createElement("p");
-      p.id = "alireza";
-      p.innerHTML = `Hello team, my name is <span id="alireza-span">${this.name}</span>
+
+  this.addToPage = function () {
+    const p = document.createElement("p");
+    p.id = "alireza";
+    p.innerHTML = `Hello team, my name is <span id="alireza-span">${this.name}</span>
         I am from timezone: ${this.timeZone}
           <span id="alireza-face">😊</span> <span id="alireza-hand">👋</span>`;
-      document.querySelector(".container").appendChild(p);
-    };
-  }
-  
-  const sushant = new Member(memberData.sushant);
-  sushant.addToPage();
-  
+    document.querySelector(".container").appendChild(p);
+  };
+}
+
+const sushant = new Member(memberData.sushant);
+sushant.addToPage();
+
+const silvia = new Member(memberData.silvia);
+silvia.addToPage();
 
 /* ***********************************************
 **********************************************
