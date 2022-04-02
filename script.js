5a68d2691b8627f087dbd6a8b83cee5c7e4077fd
Sushant Ranjan <74361863+RanjanSushant@users.noreply.github.com>
2022-04-02 16:10:10 +0530

    Created Member constructor

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ function alirezaEmojiClick(e) {
 }
 document.getElementById("alireza-face").addEventListener("click", alirezaEmojiClick)
 
+/*******************************
+ * Member Constructor
+ *******************************/
+
+ const memberData = {
+    sushant: { name: "Sushant" },
+  };
+  
+  function Member(memberDetails) {
+    Object.assign(this, memberDetails);
+  
+    this.log = function () {
+      console.log(this.name);
+    };
+  
+    this.addToPage = function () {
+      const p = document.createElement("p");
+      p.id = "alireza";
+      p.innerHTML = `Hello team, my name is <span id="alireza-span">${this.name}</span>
+          <span id="alireza-face">😊</span> <span id="alireza-hand">👋</span>`;
+      document.querySelector(".container").appendChild(p);
+    };
+  }
+  
+  const sushant = new Member(memberData.sushant);
+  sushant.addToPage();
+  
+
 /* ***********************************************
 **********************************************
                name section         
